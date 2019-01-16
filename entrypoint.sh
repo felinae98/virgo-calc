3ea11c1747a28244cd28358e09b837286981cb63
felinae98 <731499577@qq.com>
2019-01-16 13:08:00 +0800

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -ex
 if [ ! -z "$SECRET_KEY" ]; then
     SECRET_KEY=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};)
     echo "generate secret key"
-done
+fi
 
 exec gunicorn "app:app" \
     --bind "0.0.0.0:5000" \
