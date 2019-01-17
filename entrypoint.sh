31592eeb2e0e494093582909696d857647d51ea6
felinae98 <731499577@qq.com>
2019-01-17 21:44:22 +0800

    try update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-if [ ! -z "$SECRET_KEY" ]; then
+if [ -z "$SECRET_KEY" ]; then
     SECRET_KEY=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};)
     echo "generate secret key"
 fi
